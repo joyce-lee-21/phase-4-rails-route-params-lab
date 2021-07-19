f9672501b2bb69d3bf8d287f5a57f8dbfa193733
Joyce Lee <joycelee901@gmail.com>
2021-07-19 12:08:01 -0500

    commit tree 737d5a2b1245cd30d996e76001c7966c62dda199

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,20 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+
+    if params[:name]
+      students = Student.all.select {|student|
+        student.first_name == params[:name].capitalize || student.last_name == params[:name].capitalize
+      }
+    end
+
     render json: students
   end
-
+  # byebug
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
+
+
